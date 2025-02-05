529725efa003718e8a8b12c73496d6318efc86b4
okyunsu <okyunsu@gmail.com>
2025-02-05 15:17:09 +0900

    02.05 15:00

--- app.py
+++ app.py
@@ -4,27 +4,33 @@ app = Flask(__name__)
 
 @app.route('/', methods = ["GET","POST"])
 def index():
-
-    
-
-
-
-
     if request.method == "POST":
         print("POST 방식 접근")
         price = request.form.get("price")
         paid = request.form.get("paid")
-        coin500 = 2
-        coin100 = 3
-        coin50 = 0
-        coin10 = 0
         price, paid = int(price), int(paid)
-        change = price - paid
         print(f"지불 해야할 가격{price}")
         print(f"지불한 가격{paid}")
+        change = paid - price
         print(f"거스름 돈 {change}")
+        COIN_500 = 500
+        coin500 = change//COIN_500
+        coin500_nmg = change%COIN_500
+        
+        COIN_100 = 100
+        coin100 = coin500_nmg // COIN_100
+        coin100_nmg = coin500_nmg % COIN_100       
+        
+        COIN_50 = 50
+        coin50 = coin100_nmg // COIN_50
+        coin50_nmg = coin100_nmg % COIN_50
+        
+        COIN_10=10
+        coin10 = coin50_nmg // COIN_10
+        coin10_nmg = coin50_nmg % COIN_10
+    
         print(f"동전 종류와 수")
-        print(f"500원{coin500}, 100원{coin100}")
+        print(f"500원: {coin500}, 100원: {coin100}, 50원: {coin50}, 10원: {coin10}")
         return render_template("index.html", price = price, paid= paid, change = change,
                                coin500=coin500, coin100=coin100, coin50=coin50, coin10= coin10)
 
